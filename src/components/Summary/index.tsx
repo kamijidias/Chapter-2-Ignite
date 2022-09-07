3fc4d0388d20386d59ec3918ec800373726742cc
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-09-07 17:16:23 -0300

    finalizando a inserção

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext';
+import { TransactionsContext } from '../../hooks/TransactionsContext';
 
 import { Container } from "./styles";
 
--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
+import { TransactionsContext } from '../../hooks/TransactionsContext';
 import { Container } from './styles';
 
 export const TransactionsTable = () => {
