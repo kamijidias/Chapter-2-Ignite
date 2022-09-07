e00cdc540729ec5a87b6399e8846aa3cbb1f4973
Andrew Kamiji Dias <andrew_kamiji_dias@hotmail.com>
2022-09-07 15:31:43 -0300

    Carregando as trasações com o Context

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
-import { TransactionsContext } from './TransactionContext';
+import { TransactionsContext, TransactionsProvider } from './TransactionContext';
 
 Modal.setAppElement('#root');
 
@@ -21,7 +21,7 @@ export function App() {
   }
   return (
 
-    <TransactionsContext.Provider value={[]}>
+    <TransactionsProvider >
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
 
       <Dashboard />
@@ -32,7 +32,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </TransactionsContext.Provider>
+    </TransactionsProvider>
   );
 }
 
